0c470942cb7c76b3820bd197d7e23e95ea99da9a
utkavaz <ntkavkaz@rambler.ru>
2022-11-23 23:15:54 +0300

    TEST

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+int square = num*num;
+//Console.WriteLine("Квадрат введенного числа равен "+square);
+Console.WriteLine($"Квадрат введенного числа равен {square}");
+
+if (num>9 && num<100)   // проверка на двузначное число
+{
+    Console.WriteLine("вы ввели двузначное число");
+}
+
+while (num<=25)  // вывод всех чисел от введенного до 25
+{
+    Console.Write($"{num} ");
+    num++;
+}
+Console.WriteLine();
---
+++ working in groups_2/Program.cs
@@ -0,0 +1,48 @@
+﻿//Задача №3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+//3 -> Среда
+//5 -> Пятница
+
+Console.WriteLine("Введите число от 1 до 7");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 0 && num <= 7)
+{
+    if (num == 1)
+    { 
+    Console.WriteLine("понедельник");
+    }
+
+    if (num == 2)
+    {
+        Console.WriteLine("вторник");
+    }
+
+    if (num == 3)
+    {
+        Console.WriteLine("среда");
+    }
+
+    if (num == 4)
+    {
+        Console.WriteLine("четверг");
+    }
+    
+    if (num == 5)
+    {
+        Console.WriteLine("пятница");
+    }
+
+    if (num == 6)
+    {
+        Console.WriteLine("субота");
+    }
+
+    if (num == 7)
+    {
+        Console.WriteLine("воскрксенье");
+    }
+}
+else
+{
+    Console.WriteLine("вы ввели неверное число");
+}
